f12f11975a6fd4af907c1a155f067486bc7538b3
Gamidov_m <maga.gamidov.98@mail.ru>
2023-11-07 15:52:51 +0300

    first commit

---
+++ src/test/java/FormTest.java
@@ -0,0 +1,43 @@
+import com.codeborne.selenide.Condition;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class FormTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void shouldBeSuccessfullyCompleted() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Мо");
+        $$(".menu-item__control").findBy(Condition.text("Москва")).click();
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue("Рязнов-Петрочив Виктор");
+        $("[data-test-id=phone] input").setValue("+79996665588");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + planningDate));
+    }
+}
